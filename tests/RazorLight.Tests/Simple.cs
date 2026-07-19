0bee663edf4001e2bbab3a54293f174caadfa891
Jay Sanati <jaysanati@outlook.com>
2026-07-18 22:43:12 -0700

    More Cleanup

--- tests/RazorLight.Tests/Simple.cs
+++ tests/RazorLight.Tests/Simple.cs
@@ -2,66 +2,66 @@
 
 public class ViewModel
 {
-	public required string Name { get; set; }
+    public required string Name { get; set; }
 }
 
 [TestClass]
 public sealed class Simple
 {
-	[TestMethod]
-	public async Task HellowWorld()
-	{
-		var engine = new RazorLightEngineBuilder()
-			// required to have a default RazorLightProject type,
-			// but not required to create a template from string.
-			.UseEmbeddedResourcesProject(typeof(ViewModel))
-			.SetOperatingAssembly(typeof(ViewModel).Assembly)
-			.UseMemoryCachingProvider()
-			.Build();
+    [TestMethod]
+    public async Task HellowWorld()
+    {
+        var engine = new RazorLightEngineBuilder()
+            // required to have a default RazorLightProject type,
+            // but not required to create a template from string.
+            .UseEmbeddedResourcesProject(typeof(ViewModel))
+            .SetOperatingAssembly(typeof(ViewModel).Assembly)
+            .UseMemoryCachingProvider()
+            .Build();
 
-		string template = "Hello, @Model.Name. Welcome to RazorLight repository";
-		ViewModel model = new ViewModel { Name = "John Doe" };
+        string template = "Hello, @Model.Name. Welcome to RazorLight repository";
+        ViewModel model = new ViewModel { Name = "John Doe" };
 
-		string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+        string result = await engine.CompileRenderStringAsync("templateKey", template, model);
 
-		Assert.AreEqual("Hello, John Doe. Welcome to RazorLight repository", result);
-	}
+        Assert.AreEqual("Hello, John Doe. Welcome to RazorLight repository", result);
+    }
 
-	[TestMethod]
-	public async Task IfCondition()
-	{
-		var engine = new RazorLightEngineBuilder()
-			// required to have a default RazorLightProject type,
-			// but not required to create a template from string.
-			.UseEmbeddedResourcesProject(typeof(ViewModel))
-			.SetOperatingAssembly(typeof(ViewModel).Assembly)
-			.UseMemoryCachingProvider()
-			.Build();
+    [TestMethod]
+    public async Task IfCondition()
+    {
+        var engine = new RazorLightEngineBuilder()
+            // required to have a default RazorLightProject type,
+            // but not required to create a template from string.
+            .UseEmbeddedResourcesProject(typeof(ViewModel))
+            .SetOperatingAssembly(typeof(ViewModel).Assembly)
+            .UseMemoryCachingProvider()
+            .Build();
 
-		string template = "@if(Model.Name == \"John Doe\") { <text>Hello, @Model.Name. Welcome to RazorLight repository</text> }";
-		ViewModel model = new ViewModel { Name = "John Doe" };
+        string template = "@if(Model.Name == \"John Doe\") { <text>Hello, @Model.Name. Welcome to RazorLight repository</text> }";
+        ViewModel model = new ViewModel { Name = "John Doe" };
 
-		string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+        string result = await engine.CompileRenderStringAsync("templateKey", template, model);
 
-		Assert.AreEqual("Hello, John Doe. Welcome to RazorLight repository", result);
-	}
+        Assert.AreEqual("Hello, John Doe. Welcome to RazorLight repository", result);
+    }
 
-	[TestMethod]
-	public async Task IfElseCondition()
-	{
-		var engine = new RazorLightEngineBuilder()
-			// required to have a default RazorLightProject type,
-			// but not required to create a template from string.
-			.UseEmbeddedResourcesProject(typeof(ViewModel))
-			.SetOperatingAssembly(typeof(ViewModel).Assembly)
-			.UseMemoryCachingProvider()
-			.Build();
+    [TestMethod]
+    public async Task IfElseCondition()
+    {
+        var engine = new RazorLightEngineBuilder()
+            // required to have a default RazorLightProject type,
+            // but not required to create a template from string.
+            .UseEmbeddedResourcesProject(typeof(ViewModel))
+            .SetOperatingAssembly(typeof(ViewModel).Assembly)
+            .UseMemoryCachingProvider()
+            .Build();
 
-		string template = "@if(Model.Name == \"John Doe\") { <text>Hello, @Model.Name. Welcome to RazorLight repository</text> } else { <text>Hello, stranger!</text> }";
-		ViewModel model = new ViewModel { Name = "John Smith" };
+        string template = "@if(Model.Name == \"John Doe\") { <text>Hello, @Model.Name. Welcome to RazorLight repository</text> } else { <text>Hello, stranger!</text> }";
+        ViewModel model = new ViewModel { Name = "John Smith" };
 
-		string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+        string result = await engine.CompileRenderStringAsync("templateKey", template, model);
 
-		Assert.AreEqual("Hello, stranger!", result);
-	}
+        Assert.AreEqual("Hello, stranger!", result);
+    }
 }
